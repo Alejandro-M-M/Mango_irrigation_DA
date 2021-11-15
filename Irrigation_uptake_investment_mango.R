d359f484bf8be2da8074f2e6980d057190b697c3
Alejandro-M-M <84018340+Alejandro-M-M@users.noreply.github.com>
2021-11-15 14:37:47 -0600

    Add files via upload

--- Irrigation_uptake_investment_mango.R
+++ Irrigation_uptake_investment_mango.R
@@ -1,19 +1,4 @@
 
-# A function to let you test code line by line 
-make_variables <- function(est, n=1)
-  
-{ x <-random(rho=est, n=n)
-for(i in colnames(x)) assign(i, as.numeric(x[1,i]),envir=.GlobalEnv)}
-
-make_variables(estimate_read_csv("Input_table_uptake.csv"))
-
-prices_irrigation <- vv(var_mean = mango_price_irrigation, 
-                        var_CV = Var_CV,
-                        relative_trend = 2,
-                        n = 5)
-
-plot(prices_irrigation)
-
 # Housekeeping
 rm(list = ls())
 
@@ -44,39 +29,41 @@ irrigation_function <- function(){
                           relative_trend = 5,
                           n = 5)
   
-  # Risk of dry well
-  "adjusted_yield_increase <- chance_event(chance = Residue_risk, 
-                                               value_if = Active_export_per_year_containers_1MCP-2,
-                                               value_if_not = Active_export_per_year_containers_1MCP,
-                                               n = 5)"
-  
   # Profit without irrigation
   profit_no_irrigation <- prices_no_irrigation*yield_no_irrigation*farm_size
   
+
   # Profit with well
-  profit_with_well <- (prices_irrigation*yield_irrigation*farm_size)-
-    investment_cost_well-study_cost_well-(maintenance_well*5)
+  profit_well <- prices_irrigation*yield_irrigation*farm_size-maintenance_well 
+  profit_well[1] <- profit_well[1] - (investment_cost_well+study_cost_well)
   
-  # Profit with water waterpot
-  profit_with_waterpot <- (prices_irrigation*yield_irrigation*farm_size)-
-    investment_cost_waterpot*farm_size-(maintenance_waterpot*5)
+  # Risk of not finding underground water
+  profit_with_well <- chance_event(chance = chance_no_underwater, 
+                                   value_if = profit_no_irrigation - study_cost_well,
+                                   value_if_not = profit_well,
+                                   n = 5)
+  
+  # Profit with raincatch
+  profit_with_raincatch <- (prices_irrigation*yield_irrigation*farm_size)-
+    maintenance_raincatch
+  profit_with_raincatch[1] <- profit_with_raincatch[1] - investment_cost_raincatch*farm_size
   
   # Discount rate
   NPV_no_irrigation <- discount(profit_no_irrigation, discount_rate = dis_rate, calculate_NPV = TRUE)
   NPV_well <- discount(profit_with_well, discount_rate = dis_rate, calculate_NPV = TRUE)
-  NPV_waterpot <- discount(profit_with_waterpot, discount_rate = dis_rate, calculate_NPV = TRUE)
+  NPV_raincatch <- discount(profit_with_raincatch, discount_rate = dis_rate, calculate_NPV = TRUE)
   
   # Overall NPV of the decision (do - don't do)
   NPV_decision_well <- NPV_well-NPV_no_irrigation
-  NPV_decision_waterpot <- NPV_waterpot-NPV_no_irrigation
+  NPV_decision_raincatch <- NPV_raincatch-NPV_no_irrigation
   
   return(list(NPV_no_irrigation =  NPV_no_irrigation,
               NPV_well =  NPV_well, 
-              NPV_waterpot = NPV_waterpot,
+              NPV_raincatch = NPV_raincatch,
               NPV_decision_well = NPV_well - NPV_no_irrigation,
-              NPV_decision_waterpot = NPV_waterpot - NPV_no_irrigation,
+              NPV_decision_raincatch = NPV_raincatch - NPV_no_irrigation,
               Cashflow_well = profit_with_well - profit_no_irrigation,
-              Cashflow_waterpot = profit_with_waterpot - profit_no_irrigation))
+              Cashflow_raincatch = profit_with_raincatch - profit_no_irrigation))
 }
 
 # Monte Carlo simulation using the model function
@@ -88,7 +75,7 @@ irrigation_mc_simulation <- mcSimulation(estimate = as.estimate(income_estimates
 # Results of a Monte Carlo simulation for estimating the comparative 
 # profits with and without irrigation
 plot_distributions(mcSimulation_object = irrigation_mc_simulation, 
-                   vars = c("NPV_well", "NPV_waterpot", "NPV_no_irrigation"),
+                   vars = c("NPV_well", "NPV_raincatch", "NPV_no_irrigation"),
                    method = 'smooth_simple_overlay', 
                    base_size = 7)
 
@@ -96,7 +83,7 @@ plot_distributions(mcSimulation_object = irrigation_mc_simulation,
 decisionSupport::plot_distributions(mcSimulation_object = irrigation_mc_simulation, 
                                     vars = c("NPV_well",
                                              "NPV_no_irrigation",
-                                             "NPV_waterpot"),
+                                             "NPV_raincatch"),
                                     method = 'boxplot')
 
 ### Well analysis
@@ -137,11 +124,11 @@ compound_figure(mcSimulation_object = irrigation_mc_simulation,
 
 # Value of the decision for installing rain capture structures
 decisionSupport::plot_distributions(mcSimulation_object = irrigation_mc_simulation, 
-                                    vars = "NPV_decision_waterpot",
+                                    vars = "NPV_decision_raincatch",
                                     method = 'boxplot_density')
 
 # Cashflow simulation 
-plot_cashflow(mcSimulation_object = irrigation_mc_simulation, cashflow_var_name = "Cashflow_waterpot")
+plot_cashflow(mcSimulation_object = irrigation_mc_simulation, cashflow_var_name = "Cashflow_raincatch")
 
 # Projection to Latent Structure Analysis 
 pls_result_rain <- plsr.mcSimulation(object = irrigation_mc_simulation,
@@ -154,14 +141,14 @@ plot_pls(pls_result_rain, input_table = income_estimates, threshold = 0, x_axis_
 
 mcSimulation_table <- data.frame(irrigation_mc_simulation$x, irrigation_mc_simulation$y[1:5])
 
-evpi <- multi_EVPI(mc = mcSimulation_table, first_out_var = "NPV_waterpot")
+evpi <- multi_EVPI(mc = mcSimulation_table, first_out_var = "NPV_raincatch")
 
-plot_evpi(evpi, decision_vars = "NPV_decision_waterpot")
+plot_evpi(evpi, decision_vars = "NPV_decision_raincatch")
 
 # Compound figure 
 
 compound_figure(mcSimulation_object = irrigation_mc_simulation, 
                 input_table = income_estimates, plsrResults = pls_result_rain, 
-                EVPIresults = evpi, decision_var_name = "NPV_decision_waterpot", 
-                cashflow_var_name = "Cashflow_waterpot", 
+                EVPIresults = evpi, decision_var_name = "NPV_decision_raincatch", 
+                cashflow_var_name = "Cashflow_raincatch", 
                 base_size = 7)
